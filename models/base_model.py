3856bce3fb4c43166d5464770de7d72d375284fe
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-05-17 18:42:32 +0300

    task4 done!

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """
 module to define the "baseClass"
 """
-import models
+#import models
 import uuid
 from datetime import datetime
 
@@ -11,10 +11,20 @@ class BaseModel:
     """
     define all common methods and attributes of the classes
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+    def __init__(self, *args, **kwargs):
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(val, date_format))
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """
@@ -40,3 +50,26 @@ class BaseModel:
         class_name = self.__class__.__name__
         ret = "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
         return ret
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    print(my_model.id)
+    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
